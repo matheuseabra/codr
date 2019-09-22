794cd45a52138e038d1b8a592332999b0ce55767
Matheus Seabra <matheus@academicmerit.com>
2019-09-22 17:06:30 -0300

    Added follow unfollow and explore routes

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import request, render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.models import User, Post
-from app.forms import LoginForm, RegisterForm, EditProfileForm
+from app.forms import LoginForm, RegisterForm, EditProfileForm, PostForm
 from werkzeug.urls import url_parse
 from datetime import datetime
 
@@ -14,18 +14,45 @@ def before_request():
         db.session.commit()
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/feed", methods=["GET", "POST"])
 @login_required
-def index():
-    posts = Post.query.all()
-    return render_template("index.html", title="Inicio", posts=posts)
+def feed():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for("feed"))
+    page = request.args.get("page", 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config["PAGINATION_LIMIT"], False
+    )
+    return render_template("feed.html", title="Feed", form=form, posts=posts.items)
+
+
+@app.route("/explore", methods=["GET"])
+@login_required
+def explore():
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["PAGINATION_LIMIT"], False
+    )
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        "explore.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("feed"))
     form = RegisterForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -50,7 +77,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != "":
-            next_page = url_for("index")
+            next_page = url_for("feed")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
@@ -58,7 +85,7 @@ def login():
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for("index"))
+    return redirect(url_for("login"))
 
 
 @app.route("/user/<username>")
@@ -82,3 +109,35 @@ def edit_profile():
         form.username.data = current_user.username
         form.bio.data = current_user.bio
     return render_template("edit_profile.html", title="Edit Profile", form=form)
+
+
+@app.route("/follow/<username>")
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("User {} was not found".format(username))
+        return redirect(url_for("index"))
+    if user == current_user:
+        flash("You cannot follow yourself!")
+        return redirect(url_for("user", username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash("You are following {}".format(username))
+    return redirect(url_for("user", username=username))
+
+
+@app.route("/unfollow/<username>")
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("User {} not found.".format(username))
+        return redirect(url_for("index"))
+    if user == current_user:
+        flash("You cannot unfollow yourself!")
+        return redirect(url_for("user", username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash("You are not following {}.".format(username))
+    return redirect(url_for("user", username=username))
